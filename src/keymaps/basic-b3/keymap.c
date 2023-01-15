a9a91ccd7cb470e589584249b9f8f4c20d22e63a
jan-becka <janbecka@lego.com>
2023-01-15 22:34:55 +0100

    fixing shift controls

--- src/keymaps/basic-b3/keymap.c
+++ src/keymaps/basic-b3/keymap.c
@@ -4,15 +4,17 @@
 
 const custom_shift_key_t custom_shift_keys[] = {
   {KC_DOT , KC_COMM}, // Shift . is ,
-  {KC_COMM, KC_EXLM}, // Shift , is !
-  {KC_MINS, KC_EQL }, // Shift - is =
-  {KC_COLN, KC_SCLN}, // Shift : is ;
+  {KC_SLSH, KC_COLN}, // Shift / is :
+  {KC_MINS, KC_UNDS}, // Shift - is _
   {KC_LCBR, KC_RCBR}, // Shift { is }
   {KC_LBRC, KC_RBRC}, // Shift [ is ]
   {KC_PIPE, KC_BSLS}, /* Shift | is \ */
   {KC_LT  , KC_GT  }, // Shift < is >
-  {KC_PIPE, KC_BSLS}, // Shift ( is )
+  {KC_LPRN, KC_RPRN}, // Shift ( is )
+  {KC_EXLM, KC_QUES}, // Shift ! is ?
   {KC_QUOT, KC_DQT }, // Shift ' is "
+  {KC_EQL , KC_PLUS}, // Shift = is +
+  {KC_BSPC, KC_DEL }, // Shift backspace is delete
   {KC_BRIGHTNESS_UP, KC_BRIGHTNESS_DOWN}, // Shift ' is "
   {KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN}, // Shift ' is "
   {KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK} // Shift ' is "
@@ -20,9 +22,6 @@ const custom_shift_key_t custom_shift_keys[] = {
 
 uint8_t NUM_CUSTOM_SHIFT_KEYS =
     sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
-// #define _QWERTY 0
-// #define _LOWER 1
-// #define _RAISE 2
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   if (!process_custom_shift_keys(keycode, record)) { return false; }
@@ -65,27 +64,27 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_5x4(
-        TD(DANCE_1) ,KC_DOT      ,TD(DANCE_3)       ,KC_P              ,KC_Y ,KC_F ,KC_G              ,KC_C              ,KC_R ,KC_L ,
+        KC_MINS     ,KC_DOT      ,KC_SLSH           ,KC_P              ,KC_Y ,KC_F ,KC_G              ,KC_C              ,KC_R ,KC_L ,
         KC_A        ,KC_O        ,MT(MOD_LALT,KC_E) ,MT(MOD_LGUI,KC_U) ,KC_I ,KC_D ,MT(MOD_RGUI,KC_H) ,MT(MOD_RALT,KC_T) ,KC_N ,KC_S ,
         KC_BSPC     ,KC_Q        ,KC_J              ,KC_K              ,KC_X ,KC_B ,KC_M              ,KC_W              ,KC_V ,KC_Z ,
         MT(MOD_MEH,KC_SPACE) ,OSM(MOD_LSFT), OSM(MOD_RCTL), TO(1)
     ),
     [1] = LAYOUT_5x4(
-        KC_ESCAPE ,KC_AT         ,KC_HASH    ,KC_DLR        ,KC_PERC      ,KC_CIRC      ,KC_AMPR       ,KC_ASTR       ,KC_COLN      ,KC_TILD  ,
-        KC_TAB    ,KC_EXLM       ,KC_LT      ,KC_LPRN       ,KC_QUOT      ,KC_EQL       ,KC_LCBR       ,KC_LBRC       ,KC_PIPE      ,KC_ENTER ,
-        KC_BSPC   ,KC_GRV        ,KC_LPRN    ,KC_RPRN       ,KC_DQT       ,KC_QUES      ,KC_LBRC       ,KC_RBRC       ,KC_BSLS      ,TO(3)    ,
-        TO(0)     ,OSM(MOD_LSFT) ,KC_RALT       ,TO(2)
+        KC_ESCAPE ,KC_AT         ,KC_HASH    ,KC_DLR        ,KC_PERC      ,KC_CIRC      ,KC_AMPR       ,KC_UP         ,KC_ASTR      ,KC_TILD  ,
+        KC_TAB    ,KC_EXLM       ,KC_LT      ,KC_LPRN       ,KC_EQL       ,KC_SCLN      ,KC_LEFT       ,KC_DOWN       ,KC_RGHT      ,KC_ENTER ,
+        KC_BSPC   ,KC_GRV        ,KC_LCBR    ,KC_LBRC       ,KC_DQT       ,KC_QUES      ,KC_QUOT       ,KC_RBRC       ,KC_PIPE      ,TO(3)    ,
+        TO(0)     ,OSM(MOD_LSFT) ,KC_RALT    ,TO(2)
     ),
     [2] = LAYOUT_5x4(
-        KC_ESCAPE ,KC_MEDIA_PLAY_PAUSE ,KC_MEDIA_PREV_TRACK ,KC_MEDIA_NEXT_TRACK ,LSFT(KC_MINS) ,KC_MINS ,KC_7 ,KC_8 ,KC_9 ,KC_TRANSPARENT ,
-        KC_TAB    ,KC_TRANSPARENT      ,KC_AUDIO_VOL_DOWN   ,KC_AUDIO_VOL_UP     ,LSFT(KC_EQL)  ,KC_EQL  ,KC_4 ,KC_5 ,KC_6 ,KC_ENTER       ,
-        KC_DEL    ,KC_KP_ASTERISK      ,KC_BRIGHTNESS_DOWN  ,KC_BRIGHTNESS_UP    ,KC_KP_DOT     ,KC_0    ,KC_1 ,KC_2 ,KC_3 ,KC_TRANSPARENT ,
-        TO(0)     ,KC_LGUI             ,KC_RALT             ,TO(1)
+        KC_ESCAPE ,KC_DOT              ,KC_SLSH             ,KC_ASTR             ,KC_MINS       ,KC_MINS ,KC_7 ,KC_8 ,KC_9 ,KC_TRANSPARENT ,
+        KC_TAB    ,KC_EXLM             ,KC_LT               ,KC_LPRN             ,KC_EQL        ,KC_EQL  ,KC_4 ,KC_5 ,KC_6 ,KC_ENTER       ,
+        KC_DEL    ,KC_ASTR             ,KC_BRIGHTNESS_DOWN  ,KC_BRIGHTNESS_UP    ,KC_KP_DOT     ,KC_0    ,KC_1 ,KC_2 ,KC_3 ,KC_LGUI        ,
+        TO(0)     ,OSM(MOD_LSFT)       ,KC_RALT             ,TO(1)
     ),
     [3] = LAYOUT_5x4(
-        TD(DANCE_54) ,KC_F9         ,KC_F10         ,KC_F11 ,KC_F12 ,KC_BRIGHTNESS_UP  ,KC_AUDIO_VOL_UP   ,KC_MEDIA_NEXT_TRACK  ,KC_MEDIA_PLAY_PAUSE ,KC_TRANSPARENT ,
-        TD(DANCE_56) ,KC_F5         ,KC_F6          ,KC_F7  ,KC_F8  ,TD(DANCE_60)   ,MT(MOD_RGUI,KC_DOWN) ,MT(MOD_LALT,KC_UP) ,TD(DANCE_63)  ,KC_ENTER       ,
-        TD(DANCE_58) ,KC_F1         ,KC_F2          ,KC_F3  ,KC_F4  ,KC_TRANSPARENT ,KC_MS_ACCEL0         ,KC_MS_ACCEL1       ,KC_MS_ACCEL2  ,KC_NO          ,
+        TD(DANCE_54) ,KC_F9         ,KC_F10         ,KC_F11 ,KC_F12 ,KC_BRIGHTNESS_UP  ,KC_AUDIO_VOL_UP   ,KC_UP     ,KC_MEDIA_PLAY_PAUSE ,KC_TRANSPARENT ,
+        TD(DANCE_56) ,KC_F5         ,KC_F6          ,KC_F7  ,KC_F8  ,TD(DANCE_60)   ,MT(MOD_RGUI,KC_LEFT) ,MT(MOD_LALT,KC_DOWN) ,KC_RGHT  ,KC_ENTER       ,
+        TD(DANCE_58) ,KC_F1         ,KC_F2          ,KC_F3  ,KC_F4  ,KC_TRANSPARENT ,KC_MS_ACCEL0         ,KC_MEDIA_NEXT_TRACK  ,KC_MS_ACCEL2  ,KC_NO          ,
         TO(0)        ,OSM(MOD_LSFT) ,OSM(MOD_RCTL)  ,TO(1)
     )
 };
